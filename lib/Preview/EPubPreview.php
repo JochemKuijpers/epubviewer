bdbc527c093fed0bb636470d12fb36af2a9b6c70
Jochem Kuijpers <git@jochemkuijpers.nl>
2025-06-15 21:51:29 +0200

    Implement EPubPreview using external Epub dependency.

--- lib/Preview/EPubPreview.php
+++ lib/Preview/EPubPreview.php
@@ -21,16 +21,19 @@
 
 namespace OCA\Epubviewer\Preview;
 
-//.epub
-use OC\Archive\ZIP;
 use OCP\Files\File;
 use OCP\Files\FileInfo;
 use OCP\IImage;
 use OCP\Preview\IProviderV2;
-use OCP\ITempManager;
+use SebLucas\EPubMeta\EPub;
 
+/**
+ * Preview generator for .epub e-book files.
+ *
+ * @author Sebastien Marinier
+ * @author Jochem Kuijpers
+ */
 class EPubPreview implements IProviderV2 {
-	private ?ZIP $zip = null;
 
 	/**
 	 * {@inheritDoc}
@@ -40,209 +43,38 @@ class EPubPreview implements IProviderV2 {
 	}
 
 	/**
-	 * Check if a preview can be generated for $path
-	 *
 	 * {@inheritDoc}
 	 */
 	public function isAvailable(FileInfo $file): bool {
-		return true;
+		return $file->getSize() > 0;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
 	public function getThumbnail(File $file, int $maxX, int $maxY): ?IImage {
-		$image = $this->extractThumbnail($file, '');
-		if ($image && $image->valid()) {
-			return $image;
-		}
-		return null;
-	}
-
-	/**
-	 * extractThumbnail from complicated epub format
-	 */
-	private function extractThumbnail(File $file, string $path): ?IImage {
-		$tmpManager = \OC::$server->get(ITempManager::class);
-		$sourceTmp = $tmpManager->getTemporaryFile();
-
 		try {
-			$content = $file->fopen('r');
-			file_put_contents($sourceTmp, $content);
-
-			$this->zip = new ZIP($sourceTmp);
-
-			$img_data = null;
-			$contentPath = $this->getContentPath();
-			if ($contentPath) {
-				$package = $this->extractXML($contentPath);
-				if ($package) {
-					$path = $contentPath;
-					$img_src = $cover = null;
-					// Try first through <manifest>
-					$items = $package->manifest->children();
-					foreach($items as $item) {
-						if (($item['id'] == 'cover' || $item['id'] == 'cover-image') && preg_match('/image\//', (string) $item['media-type'])) {
-							$img_src = (string) $item['href'];
-							break;
-						}
-					}
-
-					// in references
-					if (!$img_src) {
-						$references = $package->guide->children();
-						foreach($references as $reference) {
-							if ($reference['type'] == 'cover' || $reference['type'] == 'title-page') {
-								$cover = (string) $reference['href'];
-								break;
-							}
-						}
-					}
-
-					// no cover ? no image ? take the first page
-					if (!$img_src && !$cover) {
-						$first_page_id = (string) $package->spine->itemref['idref'];
-						if ($first_page_id) {
-							foreach($items as $item) {
-								if ($item['id'] == $first_page_id) {
-									$cover = (string) $item['href'];
-									break;
-								}
-							}
-
-						}
-					}
-
-					// have we a "cover" file ?
-					if ($cover) {
-						// relative to container
-						$img_src = null;
-						$path = $this->resolvePath($path, $cover);
-						$dom = $this->extractHTML($path);
-						if ($dom) {
-							// search img
-							$images = $dom->getElementsByTagName('img');
-							if ($images->length) {
-								$img_src = $images[0]->getAttribute('src');
-							} else {
-								$images = $dom->getElementsByTagName('image');
-								if ($images->length) {
-									$img_src = $images[0]->getAttribute('xlink:href');
-								}
-							}
-						}
-					}// cover
-
-					// img ?
-					if ($img_src) {
-						$img_src = $this->resolvePath($path, $img_src);
-						$img_data = $this->extractFileData($img_src);
-					}
-				}
+			$epub = new EPub($file->getPath());
+			$coverInfo = $epub->getCoverInfo();
+			if (!$coverInfo["found"]) {
+				return null; // Successfully parsed, but no cover image.
 			}
-
-			// Pfff. Make a pause
-			if ($img_data) {
-				$image = new \OC_Image();
-				$image->loadFromData($img_data);
-				return $image;
-			}
-			return null;
 		} catch (\Exception $e) {
-			return null;
+			return null; // Parse error, so treat as no cover image.
 		}
-	}
 
-	/**
-	 * find the main content XML (usually "content.opf")
-	 */
-	private function getContentPath() : ?string {
-		$xml_container = $this->extractXML('META-INF/container.xml');
-		if (is_object($xml_container)) {
-			$full_path = $xml_container->rootfiles->rootfile['full-path'][0];
-			if ($full_path) {
-				return $full_path->__toString();
-			}
+		// Found a cover, so attempt to convert it to an OC_Image.
+		$image = new \OC_Image();
+		$image->loadFromData($coverInfo["data"]);
+		if (!$image->valid()) {
+			return null; // Invalid image, so don't provide a cover image.
 		}
-		return null;
-	}
 
-	/**
-	 * extract HTML from Zip path
-	 * @param string $path
-	 * @return \DOMDocument|null
-	 */
-	protected function extractHTML(string $path): \DOMDocument|null {
-		$html = $this->extractFileData($path);
-		if (is_string($html)) {
-			$dom = new \DOMDocument('1.0', 'utf-8');
-			$dom->strictErrorChecking = false;
-			if (@$dom->loadHTML($html)) {
-				return $dom;
-			}
+		// Resize the valid cover image, if necessary.
+		if ($image->width() > $maxX || $image->height() > $maxY) {
+			$image->resize(min($maxX, $maxY));
 		}
-		return null;
-	}
 
-	/**
-	 * extract XML from Zip path
-	 *
-	 * @psalm-param 'META-INF/container.xml' $path
-	 */
-	private function extractXML(string $path): \SimpleXMLElement|false|null {
-		$xml = $this->extractFileData($path);
-		if (is_string($xml)) {
-			return simplexml_load_string($xml);
-		}
-		return null;
-	}
-
-	/**
-	 * get unzipped data
-	 *
-	 * @param string $path file path in zip
-	 *
-	 * @psalm-param 'META-INF/container.xml' $path
-	 *
-	 * @return false|null|string
-	 */
-	private function extractFileData(string $path): string|false|null {
-		if ($this->zip === null) {
-			return null;
-		}
-		$fp = $this->zip->getStream($path, 'r');
-		if ($fp) {
-			$content = stream_get_contents($fp);
-			fclose($fp);
-			return $content;
-		}
-		return null;
-	}
-
-	/**
-	 * Resolve relative $relPath from $path (removes ./, ../)
-	 *
-	 * @param string $path reference path
-	 * @param string $relPath relative path
-	 * @return string
-	 */
-	private function resolvePath(string $path, string $relPath): string {
-		$path = dirname($path).'/'.$relPath;
-		$pieces = explode('/', $path);
-		$parents = [];
-		foreach($pieces as $dir) {
-			switch($dir) {
-				case '.':
-					// Don't need to do anything here
-					break;
-				case '..':
-					array_pop($parents);
-					break;
-				default:
-					$parents[] = $dir;
-					break;
-			}
-		}
-		return implode('/', $parents);
+		return $image;
 	}
 }
